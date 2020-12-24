02484027f31cbce43d19412dd561c009a93b8294
Omar <omar.garciaf247@gmail.com>
2020-12-24 01:26:02 -0500

    Adding mock requests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+describe('App', () => {
+  beforeEach(() => {
+    const app = render(<App />);
+  });
+
+  // describe('user selects')
+});
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
