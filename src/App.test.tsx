3ae7d438c735d8dd7db858bcd43ff191f1bfaf76
Omar <omar.garciaf247@gmail.com>
2020-12-25 13:29:17 -0500

    adding tests for textfield

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { configure, shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
 
-describe('App', () => {
-  beforeEach(() => {
-    const app = render(<App />);
+describe("<App />", () => {
+  configure({ adapter: new Adapter() });
+  const app = shallow(<App />);
+
+  describe("<Autocomplete.Textfield />", () => {
+    it("should be present", () => {
+      const autoCompleteTextfield = app.find("Autocomplete.Textfield");
+      expect(autoCompleteTextfield).toBeTruthy();
+    });
+
+    // it('updates search query on change', () => {
+    //   app.findAllByTestId()
+    // });
   });
-
-  // describe('user selects')
 });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// it('should set the password value on change event with trim', () => {
+//   container.find('input[type="password"]').simulate('change', {
+//     target: {
+//       value: 'somenewpassword  ',
+//     },
+//   });
+//   expect(container.find('input[type="password"]').prop('value')).toEqual(
+//     'somenewpassword',
+//   );
+// });
