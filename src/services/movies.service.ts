11bae9f79f388c141ed1c6a981e7b4f41c183ebe
Omar <omar.garciaf247@gmail.com>
2020-12-26 12:09:18 -0500

    space trimming for search

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -2,7 +2,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const OMDB_URL = `https://www.omdbapi.com/`;
 
 export const getMoviesByTitle = async (query: string) => {
-  const queryEncoded = query.replace(" ", "+");
+  const queryEncoded = query.trim().replace(" ", "+");
 
   return new Promise((resolve) => {
     fetch(
