4656d2959558ed90729fc5e0a38218d82ca4ccb1
Omar <omar.garciaf247@gmail.com>
2020-12-27 00:43:38 -0500

    style updates for movie cards

--- src/Components/MovieModal.tsx
+++ src/Components/MovieModal.tsx
@@ -46,7 +46,7 @@ const ActionsContainer = styled.div`
 const MovieInfoSection = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 0.5rem 0;
+  margin-top: 0.5rem;
 `;
 
 interface MovieModalProps {
